264c91952a8aed68d5f521c6aef76d2653d60935
samuelbaruffi <samu_baruffi@yahoo.com.br>
2021-03-19 03:20:21 +0000

    adding vpc,sg,tags

--- aws/ec2-simple/main.tf
+++ aws/ec2-simple/main.tf
@@ -7,16 +7,73 @@ terraform {
   }
 }
 
+
+
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "TerraformVPC"
+  }
+
+}
+
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "allow_tls"
+  }
+}
+
+
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "TerraformSubnet"
+  }
+}
+
+
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-038f1ca1bd58a5790"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = aws_subnet.main.id  
+  associate_public_ip_address = "true"
+
+  tags = {
+    Name = "TerraformInstance"
   }
 }
